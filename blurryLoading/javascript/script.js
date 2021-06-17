969e69f9a052c8ac48d1926b495e5deaa3cfd767
Arafat <arafat97alim@gmail.com>
2021-06-18 00:53:59 +0530

    Added blurry backgroud source codes

---
+++ blurryLoading/javascript/script.js
@@ -0,0 +1,20 @@
+let bg = document.querySelector(".bg");
+let loadText = document.querySelector(".loading-text");
+
+let load = 0;
+
+let int = setInterval(blurring, 30);
+
+function blurring() {
+    load++;
+    if (load > 99) {
+        clearInterval(int)
+    }
+    loadText.innerText = `${load}%`;
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load,0,100, 30, 0)}px)`
+
+}
+const scale = (number, inMin, inMax, outMin, outMax) => {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
