3d51ff1ad50679e5f71cdea0a0cada2776f1419e
Arafat <arafat97alim@gmail.com>
2021-06-25 01:10:30 +0530

    new feature added to my project

--- soundBoard/javascript/script.js
+++ soundBoard/javascript/script.js
@@ -9,13 +9,23 @@ sounds.forEach(sound => {
 
     btnCreate.innerText = sound; //adding text
 
+
     btnCreate.addEventListener('click', () => {
+        //! stopSong();
         document.getElementById(sound).play();
     })
 
 
     // // document.getElementById("btn").appendChild(btnCreate);
     document.getElementById("btn").appendChild(btnCreate)
-
-
 })
+
+// ! Just new Feature added - if we want to stop my previous tone and start fresh 
+// function stopSong() {
+//     sounds.forEach(sound => {
+//         const song = document.getElementById(sound);
+//         song.pause();
+//         song.currentTime = 0;
+
+//     })
+// }
