16be98fa5436784c14a38a878eb4eeffe1b7aa42
Arafat <arafat97alim@gmail.com>
2021-07-08 22:31:45 +0530

    complete navAnimation added source codes

---
+++ animatedNavBar/javascript/script.js
@@ -0,0 +1,10 @@
+//now making our project more interesting
+
+const toggle = document.getElementById("toggle");
+const nav = document.getElementById("nav");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+//bas hogaya kaam boss etna hee krna tha
