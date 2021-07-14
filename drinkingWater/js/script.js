81544ca52e03dfb95eac2c662df8a47458deab23
Arafat <arafat97alim@gmail.com>
2021-07-14 16:27:21 +0530

    updated small cups successfully with styling.

--- drinkingWater/js/script.js
+++ drinkingWater/js/script.js
@@ -1,1 +1,25 @@
-console.log("object");
+const smallCups = document.querySelectorAll(".cup-small");
+const liters = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener("click", () => highlighCup(idx));
+});
+
+function highlighCup(idx) {
+  if (
+    smallCups[idx].classList.contains("full") &&
+    !smallCups[idx].nextElementSibling.classList.contains("full")
+  ) {
+    idx--;
+  }
+  //   console.log(idx);
+  smallCups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+  });
+}
