8eee5298d0a64ac01596379c68eb4cb27d09c809
Arafat <arafat97alim@gmail.com>
2021-07-14 17:22:20 +0530

    bigCups feature added and project completed.

--- drinkingWater/js/script.js
+++ drinkingWater/js/script.js
@@ -2,13 +2,16 @@ const smallCups = document.querySelectorAll(".cup-small");
 const liters = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
+// for big cups
+updateBigCup();
 
 smallCups.forEach((cup, idx) => {
   cup.addEventListener("click", () => highlighCup(idx));
 });
 
 function highlighCup(idx) {
-  if (
+  if (idx === 7 && smallCups[idx].classList.contains("full")) idx--;
+  else if (
     smallCups[idx].classList.contains("full") &&
     !smallCups[idx].nextElementSibling.classList.contains("full")
   ) {
@@ -22,4 +25,25 @@ function highlighCup(idx) {
       cup.classList.remove("full");
     }
   });
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallCups.length;
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
 }
