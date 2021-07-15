be57da1875eb84b48b82ee91fbaf5ba80053dfca
Arafat <arafat97alim@gmail.com>
2021-07-15 18:02:56 +0530

    script file added, project completed.

--- background-slider/js/script.js
+++ background-slider/js/script.js
@@ -1,1 +1,38 @@
-console.log("Script File activated");
+const body = document.body;
+const rightBtn = document.getElementById("right");
+const leftBtn = document.getElementById("left");
+const slides = document.querySelectorAll(".slide");
+
+let activeSlide = 0;
+setBgBody();
+
+rightBtn.addEventListener("click", () => {
+  //   alert("Right button activated");
+  activeSlide++;
+  console.log(activeSlide);
+  if (activeSlide > slides.length - 1) {
+    activeSlide = 0;
+  }
+  setBgBody();
+  setActiveSlide();
+});
+
+leftBtn.addEventListener("click", () => {
+  activeSlide--;
+  console.log(activeSlide);
+  if (activeSlide < 0) {
+    activeSlide = slides.length - 1;
+  }
+  setBgBody();
+  setActiveSlide();
+});
+
+function setBgBody() {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setActiveSlide() {
+  slides.forEach((slide) => slide.classList.remove("active"));
+
+  slides[activeSlide].classList.add("active");
+}
