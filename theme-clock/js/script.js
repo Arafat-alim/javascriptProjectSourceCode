486e43d54464db8dd1e711d3656d58a8daa60268
Arafat <arafat97alim@gmail.com>
2021-07-16 18:59:45 +0530

    Script file added with lots of improvment. Finished

--- theme-clock/js/script.js
+++ theme-clock/js/script.js
@@ -1,1 +1,69 @@
-console.log("Script File added");
+const hourEl = document.querySelector(".hour");
+const minEl = document.querySelector(".minute");
+const secEl = document.querySelector(".second");
+const timeEl = document.querySelector(".time");
+const dateEl = document.querySelector(".date");
+const toggle = document.querySelector(".toggle");
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+// dark mode Adding
+toggle.addEventListener("click", (e) => {
+  const html = document.querySelector("html");
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    e.target.innerHTML = "Dark Mode";
+  } else {
+    html.classList.add("dark");
+    e.target.innerHTML = "Light Mode";
+  }
+});
+
+function setTime() {
+  const time = new Date();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const date = time.getDate();
+  const hours = time.getHours();
+  const hourForClock = hours >= 13 ? hours % 12 : hours; //converting 24 to 12 hour
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const ampm = hours >= 12 ? "PM" : "AM";
+
+  hourEl.style.transform = `translate(-50%, -100%) 
+  rotate(${scale(hourForClock, 0, 11, 0, 360)}deg)`;
+
+  minEl.style.transform = `translate(-50%, -100%) 
+  rotate(${scale(minutes, 0, 59, 0, 360)}deg)`;
+
+  secEl.style.transform = `translate(-50%, -100%) 
+  rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
+
+  timeEl.innerHTML = `${hourForClock}: ${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${ampm}`;
+
+  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
+}
+// StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
+setTime();
+setInterval(setTime, 1000);
