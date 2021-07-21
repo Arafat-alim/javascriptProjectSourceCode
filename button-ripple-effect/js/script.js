f23280922732e0ee0f93ee4c010a221278b76026
Arafat <arafat97alim@gmail.com>
2021-07-22 01:16:15 +0530

    Script FIle created succesfully. And project completed. Hurrah

--- button-ripple-effect/js/script.js
+++ button-ripple-effect/js/script.js
@@ -1,1 +1,28 @@
-console.log("Cool");
+const buttons = document.querySelectorAll(".ripple");
+
+buttons.forEach((b) => {
+  b.addEventListener("click", function (e) {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    const btnTop = e.target.offsetTop;
+    const btnLeft = e.target.offsetLeft;
+
+    const insideX = x - btnLeft;
+    const insideY = y - btnTop;
+
+    console.log(insideX, insideY);
+
+    //time to create an element named span
+    const circle = document.createElement("span");
+    circle.classList.add("circle");
+    circle.style.top = insideY + "px";
+    circle.style.left = insideX + "px";
+
+    this.appendChild(circle);
+
+    //now remove the waste added classes - style
+
+    setTimeout(() => circle.remove(), 500);
+  });
+});
