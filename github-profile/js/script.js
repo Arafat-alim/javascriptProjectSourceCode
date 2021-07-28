9239e5d08ff018957139b02e55a51ff871c9a73b
Arafat <arafat97alim@gmail.com>
2021-07-28 18:11:25 +0530

    connecting my app with an API.

--- github-profile/js/script.js
+++ github-profile/js/script.js
@@ -1,1 +1,27 @@
 console.log("Script file added");
+
+const APIURL = "https://api.github.com/users/";
+const username = "arafat-alim";
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+async function getUser(username) {
+  try {
+    const res = await axios(APIURL + username);
+    console.log(res.data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const user = search.value;
+  if (user) {
+    getUser(user);
+    search.value = "";
+  }
+});
+
+getUser(username);
